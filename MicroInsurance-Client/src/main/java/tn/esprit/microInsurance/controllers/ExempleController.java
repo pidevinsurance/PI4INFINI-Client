2bd4b72d7a9c53b696af35f2c43af6a0fbf8d994
khale <khale@DESKTOP-QSN0LM3>
2019-03-21 13:14:46 +0100

    MicroInsurance Client

---
+++ MicroInsurance-Client/src/main/java/tn/esprit/microInsurance/controllers/ExempleController.java
@@ -0,0 +1,82 @@
+package tn.esprit.microInsurance.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class ExempleController {
+
+    @FXML
+    private TextField txtname;
+
+    @FXML
+    private TextField txtlastname;
+
+    @FXML
+    private TextField txtemail;
+
+    @FXML
+    private ListView<?> listView;
+
+    @FXML
+    private Button btnremove;
+
+    @FXML
+    private Button btnallusers;
+
+    @FXML
+    private Button btnConfirm;
+
+    @FXML
+    private Button btnfree;
+
+    @FXML
+    private TextField txtsearch;
+
+    @FXML
+    private Button btnsearch;
+
+    @FXML
+    private Button btnconfirmupdating;
+
+    @FXML
+    private Button btnupdate;
+
+    @FXML
+    void AllUsers(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ConfirmAddUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ConfirmUpdating(ActionEvent event) {
+
+    }
+
+    @FXML
+    void FreeUpFields(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Remove(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Searchbyid(ActionEvent event) {
+
+    }
+
+    @FXML
+    void UpdateUser(ActionEvent event) {
+
+    }
+
+}
---
+++ MicroInsurance-Client/src/main/java/tn/esprit/microInsurance/main/Main.java
@@ -0,0 +1,28 @@
+package tn.esprit.microInsurance.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/tn/esprit/microInsurance/views/ExempleView.fxml"));
+			// BorderPane root = new BorderPane();
+			Scene scene = new Scene(root, 800, 513);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
